c8c1a4419a34f2ccea38ceb9574a4f28231ce024
Snehansu103 <snehansumishra.6103@gmail.com>
2024-08-15 19:51:42 +0530

    Delete src directory

--- src/ReduxStore/Reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import productsReducer from './productsReducer';
-import loggedUserReducer from './loggedUserReducer';
-
-export const rootReducer = combineReducers({
-    products: productsReducer,
-    loggedUser: loggedUserReducer
-})
-
